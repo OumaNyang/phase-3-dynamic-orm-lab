4493e167759c7089ac2025f9bb935cb2ad7fa863
Milton Ouma <ouma.nyang1@gmail.com>
2022-08-30 09:37:43 +0300

    done

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,87 +2,59 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def self.table_name
+    "#{self.to_s.downcase}s"
+  end
 
-    def self.table_name
-        self.to_s.downcase.pluralize
+  def self.column_names
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
     end
+    column_names.compact
+  end
 
-    def self.column_names
-        DB[:conn].results_as_hash = true
-
-        sql = "pragma table_info('#{table_name}')"
-
-        # table_info = DB[:conn].execute(sql)
-        # table_info.map do |row|
-        #     row["name"]
-        # end.compact
-        DB[:conn].execute(sql).map do |row|
-            row["name"]
-        end.compact
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
     end
+  end
 
-    def initialize(options={})
-        options.each do |property, value|
-        self.send("#{property}=", value)
-        end
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
+    values.join(", ")
+  end
 
-    def table_name_for_insert
-        self.class.table_name
-    end
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
 
-    def col_names_for_insert
-        self.class.column_names.delete_if do |col|
-            col == "id"
-        end.join(", ")
-    end
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 
-
-    def values_for_insert
-        self.class.column_names.map do |col_name|
-            # getting value for variable name
-            "'#{send(col_name)}'" unless send(col_name).nil?
-        end.compact.join(", ")
-    end
-
-
-    def save
-        sql = <<-SQL
-            INSERT INTO #{self.table_name_for_insert}
-            (#{self.col_names_for_insert})
-            VALUES (#{self.values_for_insert})
-        SQL
-
-        DB[:conn].execute(sql)
-
-        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.table_name_for_insert}")[0][0]
-    end
-
-    def self.find_by_name(name)
-        sql = <<-SQL
-            SELECT * FROM #{self.table_name}
-            WHERE name = ?
-            LIMIT 1
-        SQL
-
-        DB[:conn].execute(sql, name)
-    end
-
-    def self.find_by(hash)
-        # check if value is a number if it's text change to text formatting
-        value = hash.values.first.class == Fixnum ? hash.values.first : "'#{hash.values.first}'" 
-
-        sql = <<-SQL
-            SELECT * FROM #{self.table_name}
-            WHERE #{hash.keys.first} = #{value}
-        SQL
-
-        DB[:conn].execute(sql)
-
-        # value = hash.values.first
-        # formatted_value = value.class == Fixnum ? value : "'#{value}'"
-        # sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys.first} = #{formatted_value}"
-        # DB[:conn].execute(sql)
-    end
-  
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
+  end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,4 @@ class Student < InteractiveRecord
     self.column_names.each do |col|
         attr_accessor col.to_sym
     end
-
-
 end
